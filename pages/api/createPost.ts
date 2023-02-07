21c2600370fa593c44af00a60b45715424c5e73a
Joseph De Leon <joeseffdl@gmail.com>
2023-02-07 21:48:16 +0800

    Updated 'types' utils for prisma

--- pages/api/createPost.ts
+++ pages/api/createPost.ts
@@ -1,17 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next"
 import prisma from "../../prisma/client"
+import { PrismaPostProps } from "../../utils/types"
 
-type postProps = {
-  title: string
-}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
-    const post: postProps = JSON.parse(req.body)
+    const post: PrismaPostProps = JSON.parse(req.body)
     if (req.method === "POST") {
       if (!post.title.length)
         return res.status(400).json({ message: "Title is required" })
--- utils/types.ts
+++ utils/types.ts
@@ -2,3 +2,8 @@ export type PostProps = {
     id: string;
     title: string;
 }[]
+
+// PRISMA
+export type PrismaPostProps = {
+    title: string
+}
